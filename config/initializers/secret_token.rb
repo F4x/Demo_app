334f99eaec12debc553c7af0a4770b778dcabb0f
F4x <spirit2196@gmail.com>
2013-05-23 21:30:54 +0400

    Slow start

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Demo::Application.config.secret_token = '2def1ce16ca9c103348dd900262fddfb49337664681dedd0ee50e30164278dfcdbe656d4b5bd4bb73ca5629e197fe9e027a1515f46545632ec633b7d4135f338'
